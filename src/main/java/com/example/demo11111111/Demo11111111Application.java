2f0949053a71cc34b264346bc5a68e9f85a125a9
xyj <1556323589@qq.com>
2019-07-19 15:33:43 +0800

    your description

---
+++ src/main/java/com/example/demo11111111/Demo11111111Application.java
@@ -0,0 +1,13 @@
+package com.example.demo11111111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo11111111Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo11111111Application.class, args);
+    }
+
+}
